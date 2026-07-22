d6728399cba8313501ba72e150ec875bd335fb90
kirafoxing <kirafoxing@gmail.com>
2026-07-22 16:18:31 -0300

    feat: Added alert message and check if the input are empty

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ const btn_addTarefa = document.querySelector("#btn_addTarefa")
 const area_tarefas = document.querySelector("#area_tarefas")
 
 btn_addTarefa.addEventListener('click', () => {
-    let tarefa = document.createElement('li')
-    tarefa.textContent = input_tarefas.value
-    input_tarefas.value = ''
-    area_tarefas.appendChild(tarefa)
+    const tarefa = document.createElement('li')
+    const checkVazio = input_tarefas.value.trim()
+
+    if (checkVazio == ''){
+        alert('Digite uma tarefa')
+    } else {
+        tarefa.textContent = input_tarefas.value
+        input_tarefas.value = ''
+        area_tarefas.appendChild(tarefa)
+    }
+    
 })
