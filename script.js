87790abef3a904a3d199ceecab465e68d8efd8af
kirafoxing <kirafoxing@gmail.com>
2026-07-22 16:10:26 -0300

    feat: Added the funcionality for the button and clearing the input after

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const input_tarefas = document.querySelector("#input_tarefas")
+const btn_addTarefa = document.querySelector("#btn_addTarefa")
+const area_tarefas = document.querySelector("#area_tarefas")
+
+btn_addTarefa.addEventListener('click', () => {
+    let tarefa = document.createElement('li')
+    tarefa.textContent = input_tarefas.value
+    input_tarefas.value = ''
+    area_tarefas.appendChild(tarefa)
+})
